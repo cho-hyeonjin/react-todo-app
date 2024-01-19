1ace765cd63bdfa88506a4c31b3eb2f45459979f
cho-hyeonjin <gusls2005@gmail.com>
2024-01-19 22:15:59 +0900

    feat: 아이템 추가 기능 구현

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "1", text: "리스트 뷰 구현하기", status: "active" },
     { id: "2", text: "삭제 기능 구현하기", status: "active" },
   ]);
+
+  const handleAdd = (todo) => setTodos([...todos, todo]);
 
   return (
     <section>
@@ -13,6 +16,7 @@ export default function TodoList() {
           <li key={todo.id}>{todo.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
