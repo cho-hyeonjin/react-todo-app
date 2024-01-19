33cefc78031657fb4e3d3f6b76674fb2a52a8b1c
cho-hyeonjin <gusls2005@gmail.com>
2024-01-20 01:25:14 +0900

    refactor: todo state 초기화 useState에 콜백함수로 전달하는 방식으로 변경

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,8 +4,9 @@ import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState(readTodosFromLocalStorage());
-
+  // useState는 내부적으로 값을 기억하고 있을 뿐 변경된 값을 UI 렌더링에 반영하는 것은 또 다른 문제.
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage()); // 함수 호출한 값을 전달할 게 아니라, 콜백함수로 감써서 전달한다.
+  //                                                                        그렇게 되면 useEffect를 사용할 때처럼 초기값이 필요한 경우에 초기에 한 번만 실행된다.
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
     setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
