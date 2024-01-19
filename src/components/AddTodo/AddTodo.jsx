6cbdfd795b1d70ee63257566cac027804639ad02
cho-hyeonjin <gusls2005@gmail.com>
2024-01-20 00:08:34 +0900

    chor: AddTodo.module.css 작성

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState("");
@@ -14,14 +15,15 @@ export default function AddTodo({ onAdd }) {
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           placeholder="Todo를 추가해주세요."
           value={text}
           onChange={handleChange}
+          className={styles.input}
         />
-        <button>추가</button>
+        <button className={styles.button}>추가</button>
       </form>
     </>
   );
