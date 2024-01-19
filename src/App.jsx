92d3477925293ec6f4df376eb81931fd1d411dfa
cho-hyeonjin <gusls2005@gmail.com>
2024-01-20 00:41:11 +0900

    feat: 다크모드 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
 import { useState } from "react";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
@@ -15,7 +16,7 @@ function App() {
         // 전달하는 인수값 === 함수의 참조값인 경우 인수값 생략 가능 ∴ { setFilter } 로 표현 가능
       />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
